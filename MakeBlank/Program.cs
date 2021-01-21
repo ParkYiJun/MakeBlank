ff5c2f6f49159dfe6bdb02cc903188c781a83a25
ParkYiJun <ydlwnsj25@gmail.com>
2021-01-22 02:39:18 +0900

    ver.1
    "-" 시작 줄 모두 내용 삭제

---
+++ MakeBlank/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace MakeBlank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "방탈출후기.txt";
+            if (File.Exists(path))
+            {
+                string[] lines = File.ReadAllLines(path);
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    if (lines[i].StartsWith("-"))
+                    {
+                        lines[i] = "";
+                    }
+                }
+                File.WriteAllLines(path, lines);
+            }
+        }
+
+    }
+}
